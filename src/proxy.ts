c7639b8106c5727a9971ec20dc17bca8c7e8f886
ahosanul81 <ahosanul81@gmail.com>
2026-03-03 12:20:17 +0600

    remove proxy.ts guard

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,48 +1,49 @@
 // src/middleware.ts
 
 import { NextRequest, NextResponse } from "next/server";
-import { jwtVerify } from "jose";
+// import { jwtVerify } from "jose";
 
-const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET!);
+// const secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET!);
 
 export async function proxy(request: NextRequest) {
-  const token = request.cookies.get("accessToken")?.value;
+  // const token = request.cookies.get("accessToken")?.value;
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
-  try {
-    const { payload } = await jwtVerify(token, secret);
+  // try {
+  //   const { payload } = await jwtVerify(token, secret);
 
-    const pathname = request.nextUrl.pathname;
+  //   const pathname = request.nextUrl.pathname;
 
-    if (pathname === "/dashboard") {
-      if (payload.role === "ADMIN") {
-        return NextResponse.redirect(
-          new URL("/dashboard/admin/bookings", request.url),
-        );
-      }
+  //   if (pathname === "/dashboard") {
+  //     if (payload.role === "ADMIN") {
+  //       return NextResponse.redirect(
+  //         new URL("/dashboard/admin/bookings", request.url),
+  //       );
+  //     }
 
-      if (payload.role === "TUTOR") {
-        return NextResponse.redirect(
-          new URL("/dashboard/tutor/bookings", request.url),
-        );
-      }
-    }
+  //     if (payload.role === "TUTOR") {
+  //       return NextResponse.redirect(
+  //         new URL("/dashboard/tutor/bookings", request.url),
+  //       );
+  //     }
+  //   }
 
-    if (pathname.startsWith("/dashboard/admin") && payload.role !== "ADMIN") {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+  //   if (pathname.startsWith("/dashboard/admin") && payload.role !== "ADMIN") {
+  //     return NextResponse.redirect(new URL("/login", request.url));
+  //   }
 
-    if (pathname.startsWith("/dashboard/tutor") && payload.role !== "TUTOR") {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+  //   if (pathname.startsWith("/dashboard/tutor") && payload.role !== "TUTOR") {
+  //     return NextResponse.redirect(new URL("/login", request.url));
+  //   }
 
-    return NextResponse.next();
-  } catch (error) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  //   return NextResponse.next();
+  // } catch (error) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
+  return NextResponse.next();
 }
 export const config = {
   matcher: ["/dashboard/:path*"],
